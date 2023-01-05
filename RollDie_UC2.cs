d9523009b46c860cb8dd83389aefaf4dfc27501e
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-01-05 13:18:34 +0530

    Add files via upload

---
+++ RollDie_UC2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class RollDie
+    {
+        public static void RollTheDie()
+        {
+
+            int Start_Position = 1;
+
+            Random  random= new Random();
+            int rollDie =random.Next(1,6);
+
+            Console.WriteLine("Player Roll The Die with number = " +  rollDie);
+
+
+        }
+    }
+}
