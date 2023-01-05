983b1cf632fad52013acad730aa61765d97c6683
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-01-05 13:19:56 +0530

    Add files via upload

---
+++ Player_Moves.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Player_Moves
+    {
+        public static void Player_Moves_Position()
+        {
+            Random random = new Random();
+            int rollDie = random.Next(1, 6);
+
+            Console.WriteLine("Player Roll The Die with number = " + rollDie);
+
+            int rollPosition = 0;
+
+            if (rollDie!=rollPosition )
+            {
+                Console.WriteLine("Player Not Roll The Die with stay in the same position");
+            }
+            //else if(rollDie==rollPosition)
+            //{
+               // Console.WriteLine("The player moves ahead by the number of position received in the die");
+            //}
+
+
+
+        }
+    }
+}
