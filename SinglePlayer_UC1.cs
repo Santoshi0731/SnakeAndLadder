998716506fdc5e584709f737eb94e9b5592d7948
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-01-05 13:15:36 +0530

    Add files via upload

---
+++ SinglePlayer_UC1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SinglePlayer_UC1
+    {
+        public static void Position()
+        {
+            int Start_Position = 1 ;
+
+            Random random= new Random();
+            int positionCheck = random.Next(2);
+
+            if (positionCheck == Start_Position)
+            {
+                Console.WriteLine("player Position Start with 0");
+            }
+            else
+            {
+                Console.WriteLine("player position with next step");
+            }
+        }
+          
+    }
+}
